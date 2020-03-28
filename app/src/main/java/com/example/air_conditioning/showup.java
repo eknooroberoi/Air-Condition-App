35e5a59a414f97088279ad0e8326804e0bb2ca7f
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-03-28 20:19:17 +0530

    "made contact us page"

--- app/src/main/java/com/example/air_conditioning/showup.java
+++ app/src/main/java/com/example/air_conditioning/showup.java
@@ -3,7 +3,11 @@ package com.example.air_conditioning;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
@@ -12,6 +16,12 @@ public class showup extends AppCompatActivity {
 
     DrawerLayout dl;
     NavigationView nv;
+    TextView eknoor;
+    TextView sharath;
+    TextView raheem;
+    TextView eknoormail;
+    TextView sharathmail;
+    TextView raheemmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,72 @@ public class showup extends AppCompatActivity {
         dl=findViewById(R.id.drawer_layout);
         dl=findViewById(R.id.nav_view);
         dl=findViewById(R.id.drawer_layout);
+        eknoor=findViewById(R.id.textView5);
+        sharath=findViewById(R.id.textView6);
+        raheem=findViewById(R.id.textView7);
+        eknoormail=findViewById(R.id.textView8);
+        sharathmail=findViewById(R.id.textView9);
+        raheemmail=findViewById(R.id.textView10);
+
+        eknoor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone=eknoor.getText().toString();
+                String s= "tel:+91" + phone;
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(s));
+                startActivity(intent);
+            }
+        });
+        sharath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone=sharath.getText().toString();
+                String s= "tel:+91" + phone;
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(s));
+                startActivity(intent);
+            }
+        });
+        raheem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone=raheem.getText().toString();
+                String s= "tel:+91" + phone;
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(s));
+                startActivity(intent);
+            }
+        });
+        eknoormail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone=eknoormail.getText().toString();
+                String s= "mailto:" + phone;
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse(s));
+                startActivity(intent);
+            }
+        });
+        sharathmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone=sharathmail.getText().toString();
+                String s= "mailto:" + phone;
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse(s));
+                startActivity(intent);
+            }
+        });
+        raheemmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone=raheemmail.getText().toString();
+                String s= "mailto:" + phone;
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse(s));
+                startActivity(intent);
+            }
+        });
     }
 }
