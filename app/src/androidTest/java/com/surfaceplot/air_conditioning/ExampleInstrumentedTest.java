406d9ff36b1a9ee7ad3e9c80cb7f286267b04971
thesct22 <sharathct22@gmail.com>
2020-03-29 15:36:46 +0530

    Prepared for playstore

--- app/src/androidTest/java/com/surfaceplot/air_conditioning/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/surfaceplot/air_conditioning/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.air_conditioning", appContext.getPackageName());
+        assertEquals("com.surfaceplot.air_conditioning", appContext.getPackageName());
     }
 }
