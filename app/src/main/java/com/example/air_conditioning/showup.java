45839710d5a3bf7cdf203c070f38e6a8d726b549
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-03-29 19:16:21 +0530

    "final commit"

--- app/src/main/java/com/example/air_conditioning/showup.java
+++ app/src/main/java/com/example/air_conditioning/showup.java
@@ -1,21 +1,27 @@
 package com.example.air_conditioning;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class showup extends AppCompatActivity {
+public class showup extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout dl;
     NavigationView nv;
+    Toolbar tb;
     TextView eknoor;
     TextView sharath;
     TextView raheem;
@@ -27,9 +33,17 @@ public class showup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showup);
-        dl=findViewById(R.id.drawer_layout);
-        dl=findViewById(R.id.nav_view);
-        dl=findViewById(R.id.drawer_layout);
+
+        dl = findViewById(R.id.drawer_layoutcontactus);
+        nv = findViewById(R.id.nav_viewcontactus);
+        tb = findViewById(R.id.toolbarcontactus);
+
+        setSupportActionBar(tb);
+        nv.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dl, tb, R.string.nd_open, R.string.nd_close);
+        dl.addDrawerListener(toggle);
+        toggle.syncState();
+
         eknoor=findViewById(R.id.textView5);
         sharath=findViewById(R.id.textView6);
         raheem=findViewById(R.id.textView7);
@@ -85,6 +99,7 @@ public class showup extends AppCompatActivity {
                 sendMail3();
             }
         });
+        nv.setNavigationItemSelectedListener(this);
     }
     private void sendMail1(){
         String recepient= eknoormail.getText().toString();
@@ -109,5 +124,34 @@ public class showup extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_EMAIL,recepientlist);
         intent.setType("message/rfc822");
         startActivity(Intent.createChooser(intent, " choose an email client"));
+    }
+    @Override
+    public void onBackPressed() {
+        if (dl.isDrawerOpen(GravityCompat.START)) {
+            dl.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(showup.this, MainActivity.class));
+                break;
+            case R.id.xycoord:
+                startActivity(new Intent(showup.this, Xcoord.class));
+                break;
+            case R.id.surfaceplot:
+                startActivity(new Intent(showup.this, Ycoord.class));
+                break;
+            case R.id.contactus:
+                startActivity(new Intent(showup.this, showup.class));
+                break;
+        }
+        return true;
     }
 }
