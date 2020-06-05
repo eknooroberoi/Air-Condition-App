a31101d05cd1609ef24d5af3763652dc9ac720e5
thesct22 <sharathct22@gmail.com>
2020-06-05 23:23:21 +0530

    Exiting Application Issue Solved

--- app/src/main/java/com/surfaceplot/air_conditioning/Ycoord.java
+++ app/src/main/java/com/surfaceplot/air_conditioning/Ycoord.java
@@ -3,11 +3,13 @@ package com.surfaceplot.air_conditioning;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -106,7 +108,20 @@ public class Ycoord extends AppCompatActivity implements NavigationView.OnNaviga
         if (dl.isDrawerOpen(GravityCompat.START)) {
             dl.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Closing Activity")
+                    .setMessage("Are you sure you want to close this activity?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finishAffinity();;
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
         }
     }
 
